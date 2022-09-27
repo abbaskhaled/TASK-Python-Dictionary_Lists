8f40f9814a23336f5cb676abb8075c53dd86435f
abbaskhaled <abbasq8q@gmail.com>
2022-09-27 16:22:24 +0300

    solve question 2

--- functions.py
+++ functions.py
@@ -5,7 +5,8 @@ print(books[0])
 #  recieves a book dictionary
 #  returns the number of authors that the book has
 def number_of_authors(book):
-    ...
+    return len(book["authors"]) 
+
 
 
 print(number_of_authors(books[0]))
@@ -15,7 +16,9 @@ print(number_of_authors(books[0]))
 #  # recieves a list of book dictionaries
 #  # returns the book dictionary with the same id as the book_id provided
 def get_book_by_id(book_id, books):
-    ...
+    for book in books:
+        if book_id == book["id"]:
+            return book
 
 
 print(get_book_by_id(38, books))
@@ -27,7 +30,9 @@ print(get_book_by_id(38, books))
 # adds the summary to the book dictionary
 # return the book dictionary
 def add_summary_to_book(summary, book):
-    ...
+    for book in books:
+        book["summary"] = summary
+        return book
 
 
 print(add_summary_to_book("this is a good book about", books[0]))
